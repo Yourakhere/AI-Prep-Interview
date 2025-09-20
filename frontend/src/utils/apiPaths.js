fedac8b60c312a5401a0997fc5816be1848141c6
Abhishek kumar <139471330+Yourakhere@users.noreply.github.com>
2025-09-20 21:51:23 +0530

    Update apiPaths.js

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,4 +1,7 @@
-export const BASE_URL = "https://ai-prep-interview.onrender.com";
+
+//export const BASE_URL = "https://ai-prep-interview.onrender.com";
+
+export const BASE_URL = "https://ai-prep-backend.vercel.app";
 
 export const API_PATHS = {
   AUTH: {
@@ -29,4 +32,5 @@ export const API_PATHS = {
     UPDATE_NOTE: (id) => `/api/questions/${id}/note`,  
   },
 };
+
 
