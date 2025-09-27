7115cd3f5d937cfdb445ab40cb6c39cee9cfa3e7
Abhishek kumar <139471330+Yourakhere@users.noreply.github.com>
2025-09-27 20:48:48 +0530

    Update Login.jsx
    add loader

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,57 +1,59 @@
-import React, { useContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Input from '../../components/Inputs/Input';
-import axiosInstance from '../../utils/axiosInstance';
-import { API_PATHS } from '../../utils/apiPaths';
-import { validateEmail } from '../../utils/helper';
-import { UserContext } from '../../context/userContext';
-import toast from 'react-hot-toast';
+import React, { useContext, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Input from '../../components/Inputs/Input'
+import axiosInstance from '../../utils/axiosInstance'
+import { API_PATHS } from '../../utils/apiPaths'
+import { validateEmail } from '../../utils/helper'
+import { UserContext } from '../../context/userContext'
+import toast from 'react-hot-toast'
+import SpinnerLoader from "../../components/Loader/SpinnerLoader";
 
 const Login = ({ setCurrentPage }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+  const [isLoading, setIsLoading] = useState(false);
+  const { updateUser } = useContext(UserContext)
 
-  const { updateUser } = useContext(UserContext);
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const handleLogin = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     if (!validateEmail(email)) {
-      toast.error('Please enter a valid email address');
-      return;
+      toast.error('Please enter a valid email address')
+      return
     }
     if (!password) {
-      toast.error('Please enter the password');
-      return;
+      toast.error('Please enter the password')
+      return
     }
 
     try {
       const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
         email,
         password,
-      });
-      const { token } = response.data;
+      })
+      const { token } = response.data
 
       if (token) {
-        localStorage.setItem('token', token);
-        updateUser(response.data);
-        navigate('/dashboard');
+        localStorage.setItem('token', token)
+        updateUser(response.data)
+        navigate('/dashboard')
       }
     } catch (error) {
       if (error.response && error.response.data) {
-        toast.error(error.response.data.message);
+        toast.error(error.response.data.message)
       } else {
-        toast.error('Something went wrong, please try again later');
+        toast.error('Something went wrong, please try again later')
       }
     }
-  };
+  }
 
   return (
-    <div className="w-[90vw] md:w-[33vw] p-8 flex flex-col justify-center bg-white rounded-2xl shadow-lg">
-      <h3 className="text-2xl font-bold text-purple-900 mb-1">Welcome Back</h3>
-      <p className="text-gray-700 mb-6">Please enter your details to login</p>
+    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
+      <h3 className="text-lg font-semibold text-black">Welcome Back</h3>
+      <p>Please enter your details to login</p>
       <form onSubmit={handleLogin}>
         <Input
           value={email}
@@ -68,25 +70,30 @@ const Login = ({ setCurrentPage }) => {
           type="password"
         />
         {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+        
         <button
           type="submit"
-          className="w-full py-3 mt-4 mb-3 rounded-full bg-gradient-to-r from-purple-500 to-pink-500 text-white font-semibold hover:from-purple-600 hover:to-pink-600 transition-all"
+          className="btn-primary w-full mt-2"
+          disabled={isLoading}
         >
-          LOGIN
+           {isLoading ? <SpinnerLoader /> : "LOGIN"}
+
         </button>
-        <p className="text-[13px] text-gray-600 mt-3">
+        <p className="text-[13px] text-slate-800 mt-3">
           Don't have an account?{' '}
           <button
             type="button"
-            className="font-medium text-purple-600 hover:text-pink-500 underline cursor-pointer"
-            onClick={() => setCurrentPage('signup')}
+            className="font-medium text-primary underline cursor-pointer"
+            onClick={() => {
+              setCurrentPage('signup')
+            }}
           >
             Sign Up
           </button>
         </p>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
